f4660afa3e30b21ef5c47f29032283a9c76d0007
Luis Vasquez <levasquez00@gmail.com>
2026-03-26 20:07:51 -0600

    testing variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,11 @@
+//use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let x: i32 = 5;
+    let y: i32 = 10;
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The sum of x and y is: {}", x + y);
 }
